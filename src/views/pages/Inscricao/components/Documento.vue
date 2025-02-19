134ad180f3ffaf657e8afc8be417d1e2c708175d
Diego Matheus <35178863+DiegoMPMS@users.noreply.github.com>
2025-02-19 14:51:09 -0300

    Todo conteúdo da página foi removido para testar a submissão do cadastro usando a api

--- src/views/pages/Inscricao/components/Documento.vue
+++ src/views/pages/Inscricao/components/Documento.vue
@@ -4,111 +4,12 @@
 	<v-form v-model="form" @submit.prevent="onSubmit">
 
 		<v-container>
-			<v-row>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.doc_pessoal_com_foto" label="Documento pessoal com foto"
-						accept="application/.pdf" :rules="[rules.required, rules.file_size_limit]" clearable show-size
-						hint="* obrigatório" persistent-hint>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								<ul class="mx-2">
-									<li>Carteira de identidade</li>
-									<li>Carteira de motorista</li>
-									<li>Certificado de reservista</li>
-									<li>Carteira de trabalho</li>
-									<li>Passaporte</li>
-									<li>Identidade funcional emitida por órgão de classe</li>
-								</ul>
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.cert_quitacao_eleitoral" label="Certidão de quitação eleitoral"
-						accept="application/.pdf" :rules="[rules.required, rules.file_size_limit]" clearable show-size
-						hint="* obrigatório" persistent-hint>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								Para mais informações sobre documentos aceitos, consulte o edital XX.X
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-			</v-row>
-
-			<v-row>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.certidao_antecedentes_criminais"
-						label="Certidão de antecedentes criminais" accept="application/.pdf"
-						:rules="[rules.required, rules.file_size_limit]" clearable show-size hint="* obrigatório" persistent-hint>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								Para mais informações sobre documentos aceitos, consulte o edital XX.X
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.reservista_dispensa"
-						label="Certificado de reservista / Dispensa de incorporação" accept="application/.pdf"
-						:rules="[rules.file_size_limit]" clearable show-size>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								Para mais informações sobre documentos aceitos, consulte o edital XX.X
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-			</v-row>
-
-			<v-row>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.relatorio_medico" label="Relatório médico" accept="application/.pdf"
-						:rules="[rules.file_size_limit]" clearable show-size>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								Para mais informações sobre documentos aceitos, consulte o edital XX.X
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-				<v-col cols="12" sm="6">
-					<v-file-input v-model="store.arquivos.declaracao_afrodescendencia"
-						label="Certidão de auto declaração de afrodescendência" accept="application/.pdf"
-						:rules="[rules.file_size_limit]" clearable show-size>
-						<template v-slot:append-inner>
-							<v-tooltip location="bottom">
-								<template v-slot:activator="{ props }">
-									<v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-								</template>
-								Para mais informações sobre documentos aceitos, consulte o edital XX.X
-							</v-tooltip>
-						</template>
-					</v-file-input>
-				</v-col>
-
-			</v-row>
+			
 		</v-container>
 
 		<v-btn :disabled="!form" :loading="loading" color="success" size="large" type="submit" block>Cadastrar</v-btn>
 
 	</v-form>
-
 
 </template>
 
@@ -120,7 +21,7 @@ const store = useCadastroStore();
 <script>
 export default {
 	data: () => ({
-		form: false,
+		form: true,
 		// cada field do formulário deve conter sua vária declara no script
 		// o nome do variável é 'linkado' usando v-model="nome_da_variável"
 
@@ -143,14 +44,33 @@ export default {
 		store: () => useCadastroStore()
 	},
 	methods: {
-		onSubmit() {
+		get_cadastro_info() {
+			// retorna apenas os campos utilizados no cadastro do usuário
+			// em formato de objeto
+			const static_store = JSON.parse(JSON.stringify(this.store.$state));
+			delete static_store.arquivos;
+			delete static_store.step;
+			console.log(JSON.stringify(static_store));
+			return static_store
+		},
+		async onSubmit() {
 			if (!this.form) return
 
 			this.loading = true
+
+			const url = 'http://localhost:8000/api/cadastro'
+			const response = await fetch(url, {
+				method:"POST",
+				headers: {"Content-type": "application/json;charset=UTF-8"},
+				body: JSON.stringify(this.get_cadastro_info())
+			});
+			console.log('------------ RESPONSE --------------');
+			console.log(response.json());
 			// aqui deveria estar a chama da API para realizar o cadastro com os dados iniciais, e a espera pela resposta para seguir ao próximo passo 
 			// e provavelmente um acesso ao VUE storage pra guardar as informações caso seja necessário voltar ou reiniciar o formulário
 			// por enquanto esperamos alguns segundos e seguimos para a próxima página.
-			setTimeout(() => (this.$router.push('cadastro3')), 2000);
+			//setTimeout(2000);
+			this.loading = true
 		},
 		// async para enviar documentos, devido ao possível limite de post de 2megabytes,
 		// cada arquivo tera um limite de 2.000 bytes e o resto do post deve caber no espaço restante
